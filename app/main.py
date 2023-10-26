462d184f1a7cb7c4484685c75325be018e97ab50
Greg <greg.herringer@gmail.com>
2023-10-26 12:08:51 -0700

    add programmatic logic to run app for easier development testing

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from app.api import api_handlers
 
@@ -9,3 +10,6 @@ app.include_router(api_handlers.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to the URL Shortener App, the free and easy way to shorten long and unseemly URLs."}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", port=8000, reload=True)
