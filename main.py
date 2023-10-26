0fdbfda1ca51db5b658ddd63997323bc0647860b
Greg <greg.herringer@gmail.com>
2023-10-25 22:32:10 -0700

    implement shorten_url outline with placeholder functionality, create Record class using pydantic BaseModel

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import uuid4
+
+
+class Record(BaseModel):
+    original_url: str
+    custom_url: str | None = None
+
+
+SHORT_URL_LENGTH = 8
 
 app = FastAPI()
+
+
+def post_to_db(foo = None, bar = None):
+    # placeholder function
+    return
 
 
 @app.get("/")
@@ -13,5 +28,20 @@ async def root():
 
 
 @app.post("/shorten_url")
-async def shorten_url():
-    pass
+async def shorten_url(record: Record):
+    if record.custom_url:
+        # Custom url provided, proceed with posting to DB
+        db_response = post_to_db(record.custom_url, record.original_url)
+
+    else:
+        # No custom url provided, need to generate random short url
+        while True:
+            random_full_url = uuid4()
+            random_short_url = random_full_url
+            # random_short_url = str(random_full_url)[:SHORT_URL_LENGTH]
+            db_response = post_to_db(random_short_url, record.original_url)
+            if not db_response:
+                # placeholder if statement
+                break
+
+    return db_response
