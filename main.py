04a87af74509516277c365a384c88363eb92b006
Greg <greg.herringer@gmail.com>
2023-10-25 23:02:01 -0700

    update test client params

--- main.py
+++ main.py
@@ -79,9 +79,15 @@ async def redirect(record: Record):
 # --------------------------------------------------------------------------------------------- #
 def test_api():
     res_home = client.get("/")
+
     res_no_params = client.post(url="/shorten_url", json={})
-    res_params1 = client.post(url="/shorten_url", json={"original_url": "http://www.original.com"})
-    res_params2 = client.post(url="/shorten_url", json={"original": "http://www.original.com", "custom": "thegoods"})
+
+    res_params1 = client.post(url="/shorten_url",
+                              json={"original_url": "http://www.original.com"})
+
+    res_params2 = client.post(url="/shorten_url",
+                              json={"original_url": "http://www.original.com",
+                                    "custom_url": "thegoods"})
 
     results = [res_home, res_no_params, res_params1, res_params2]
     for r in results:
