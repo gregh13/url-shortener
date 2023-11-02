e4a28e7efbea72fa877066b3880e891cde1e1f04
Greg <greg.herringer@gmail.com>
2023-11-01 17:32:24 -0700

    add basic check for items in database

--- app/models/pynamo_models.py
+++ app/models/pynamo_models.py
@@ -15,4 +15,21 @@ class Thread(Model):
         read_capacity_units = 5
 
     short_url = UnicodeAttribute(hash_key=True, attr_name="ShortUrl")
-    original_url = UnicodeAttribute(range_key=True, attr_name="OriginalUrl")
+    original_url = UnicodeAttribute(attr_name="OriginalUrl")
+
+# # Item creation, already done
+# new_item = Thread(short_url='ex_short', original_url="ex_reallllly_looonnngg")
+# new_item.save()
+
+check_item_in_db = Thread.get("ex_short")
+print(f"Item pulled from local database: \n"
+      f"Short Url: {check_item_in_db.short_url}\n"
+      f"Original Url: {check_item_in_db.original_url}\n")
+
+
+try:
+    fake_url = "my_fake_url"
+    check_fake_item = Thread.get(fake_url)
+    print(check_fake_item)
+except Thread.DoesNotExist:
+    print(f"'{fake_url}' does not exist in database")
