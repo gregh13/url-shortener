4f3fba5b6fda7974e1caf731645452c08f64bd1c
Greg <greg.herringer@gmail.com>
2023-11-07 08:52:58 -0800

    implement tests for service/get_one_url function

--- app/tests/unit/test_service_get_one_url.py
+++ app/tests/unit/test_service_get_one_url.py
@@ -1,9 +1,28 @@
 import unittest
+from app.service.database import get_one_url
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class GetOneUrl(unittest.TestCase):
+    def test_database_reachable(self):
+        response = get_one_url("test")
+        self.assertNotEquals(response["status_code"], 500)
+
+    def test_url_does_not_exist(self):
+        response = get_one_url("nonexistant")
+        self.assertEqual(response["status_code"], 404)
+
+    def test_url_exists(self):
+        response = get_one_url("existing_url")
+        self.assertEqual(response["status_code"], 200)
+
+        # self.assertEqual(response["payload"], "https://www.exisiting_url.com")
+
+    def test_bad_url_inputs(self):
+        all_bad_input = [set(), {}, {1}, {"url": "test"}, "*(#&$)(&@(#", 3.1415, ["testurl"], (1), True, False, None]
+
+        for bad_input in all_bad_input:
+            response = get_one_url(bad_input)
+            self.assertEqual(response, 400)
 
 
 if __name__ == '__main__':
