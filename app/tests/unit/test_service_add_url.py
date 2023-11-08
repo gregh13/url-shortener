d2b16ba421d8759de71ed773d8b1687d7a9421b7
Greg <greg.herringer@gmail.com>
2023-11-07 16:58:21 -0800

    update test_database_reachable

--- app/tests/unit/test_service_add_url.py
+++ app/tests/unit/test_service_add_url.py
@@ -1,9 +1,17 @@
 import unittest
+import uuid
+from app.service.database import add_url_to_db, delete_item
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestAddUrl(unittest.TestCase):
+    def test_database_reachable(self):
+        random_string = str(uuid.uuid4())
+        add_response = add_url_to_db(short_url=random_string, original_url="testing_database")
+        self.assertNotEqual(500, add_response["status_code"])
+
+        # Delete item just added to DB
+        del_response = delete_item(random_string)
+        self.assertEqual(200, del_response["status_code"])
 
 
 if __name__ == '__main__':
