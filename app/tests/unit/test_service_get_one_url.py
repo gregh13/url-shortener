4cb33053c0ab1ca74161ed105c342a8924d353c9
Greg <greg.herringer@gmail.com>
2023-11-07 16:02:57 -0800

    switch assertion parameter order

--- app/tests/unit/test_service_get_one_url.py
+++ app/tests/unit/test_service_get_one_url.py
@@ -5,15 +5,15 @@ from app.service.database import get_one_url
 class TestGetOneUrl(unittest.TestCase):
     def test_database_reachable(self):
         response = get_one_url("test")
-        self.assertNotEqual(response["status_code"], 500)
+        self.assertNotEqual(500, response["status_code"])
 
     def test_url_does_not_exist(self):
         response = get_one_url("nonexistant")
-        self.assertEqual(response["status_code"], 404)
+        self.assertEqual(404, response["status_code"])
 
     def test_url_exists(self):
         response = get_one_url("existing_url")
-        self.assertEqual(response["status_code"], 200)
+        self.assertEqual(200, response["status_code"])
 
         # self.assertEqual(response["payload"], "https://www.exisiting_url.com")
 
