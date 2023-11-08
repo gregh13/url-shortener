5e1904e763bbc3a4d5c20fc315f08c167b2271fb
Greg <greg.herringer@gmail.com>
2023-11-07 16:02:10 -0800

    implement tests for service/get_all_urls function

--- app/tests/unit/test_service_get_all_urls.py
+++ app/tests/unit/test_service_get_all_urls.py
@@ -1,9 +1,15 @@
 import unittest
+from app.service.database import get_all_urls
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestGetAllUrls(unittest.TestCase):
+    def test_database_reachable(self):
+        response = get_all_urls()
+        self.assertNotEqual(500, response["status_code"])
+
+    def test_successful_retrieval(self):
+        response = get_all_urls()
+        self.assertEqual(200, response["status_code"])
 
 
 if __name__ == '__main__':
