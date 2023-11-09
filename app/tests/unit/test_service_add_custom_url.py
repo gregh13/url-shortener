53a5645db82fe9a698136b57e4aa24b2d65a8485
Greg <greg.herringer@gmail.com>
2023-11-09 15:34:54 -0800

    implement add_custom_url tests

--- app/tests/unit/test_service_add_custom_url.py
+++ app/tests/unit/test_service_add_custom_url.py
@@ -1,9 +1,38 @@
 import unittest
+import uuid
+from app.service.database import add_custom_url_to_db, delete_item
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestAddCustomUrl(unittest.TestCase):
+    def test_url_no_collision(self):
+        mock_custom_string = str(uuid.uuid4())
+        add_response = add_custom_url_to_db(custom_url=mock_custom_string, original_url="testing_database")
+        self.assertEqual(200, add_response["status_code"])
+
+        # Delete item just added to DB
+        del_response = delete_item(mock_custom_string)
+        self.assertEqual(200, del_response["status_code"])
+
+    def test_url_collision(self):
+        response = add_custom_url_to_db(custom_url="existing_url", original_url="https://www.exisiting_url.com")
+        self.assertEqual(409, response["status_code"])
+
+    def test_bad_url_inputs(self):
+        bad_inputs_1 = [{}, {"url": "test"}, ["testurl"], ("url", "test")]
+        bad_inputs_2 = [set(), {1}, {"url"}, None]
+        all_bad_inputs = [(409, bad_inputs_1), (400, bad_inputs_2)]
+
+        for error_code, bad_input_list in all_bad_inputs:
+            for bad_input in bad_input_list:
+                response = add_custom_url_to_db(custom_url=bad_input, original_url="test.com")
+                message = f"{bad_input} --> {response["status_code"]}, {response["payload"]}"
+
+                if response["status_code"] == 200:
+                    # Delete item in case bad_input was actually added to db
+                    del_response = delete_item(bad_input)
+                    self.assertEqual(200, del_response["status_code"])
+
+                self.assertEqual(error_code, response["status_code"], msg=message)
 
 
 if __name__ == '__main__':
