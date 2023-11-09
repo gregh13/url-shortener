674f5b6ad58c5b9662164e53127a287d7c2539e3
Greg <greg.herringer@gmail.com>
2023-11-09 15:28:16 -0800

    implement add_random_url test

--- app/tests/unit/test_service_add_random_url.py
+++ app/tests/unit/test_service_add_random_url.py
@@ -1,9 +1,12 @@
 import unittest
+import uuid
+from app.service.database import add_random_url_to_db, delete_item
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestAddRandomUrl(unittest.TestCase):
+    def test_random_url(self):
+        response = add_random_url_to_db(original_url="https://www.exisiting_url.com")
+        self.assertEqual(200, response["status_code"])
 
 
 if __name__ == '__main__':
