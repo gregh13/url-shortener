6c14b65fa75887b7ca5f841689d54cd2d7eaf1ab
Greg <greg.herringer@gmail.com>
2023-11-10 15:40:09 -0800

    implement list_urls integration test

--- app/tests/integration/test_integration_api_list_urls.py
+++ app/tests/integration/test_integration_api_list_urls.py
@@ -1,9 +1,20 @@
 import unittest
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestListUrlsIntegrationAPI(unittest.TestCase):
+    def test_list_urls(self):
+        response_raw = client.get(url="/api/list_urls")
+        response = response_raw.json()
+        self.assertEqual(200, response["status_code"])
+
+        url_list = response["payload"]
+        self.assertIsInstance(url_list, list)
+
+        self.assertGreater(len(url_list), 0)
 
 
 if __name__ == '__main__':
