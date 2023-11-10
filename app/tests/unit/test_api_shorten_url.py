385d68af906a6198a7c3a0b78bc1d0be892792b2
Greg <greg.herringer@gmail.com>
2023-11-09 16:05:16 -0800

    fix response formatting

--- app/tests/unit/test_api_shorten_url.py
+++ app/tests/unit/test_api_shorten_url.py
@@ -10,13 +10,13 @@ client = TestClient(app)
 class TestShortenUrlAPI(unittest.TestCase):
     def test_no_custom_url(self):
         response = client.post(url="/api/shorten_url", json={"original_url": "https://www.existing.com"})
-        response_code = int(response[:3])
+        response_code = int(response.json()[:3])
         self.assertEqual(200, response_code)
 
     def test_custom_url_no_collision(self):
         mock_custom_string = str(uuid.uuid4())
         response = client.post(url="/api/shorten_url", json={"custom_url": mock_custom_string, "original_url": "https://www.existing.com"})
-        response_code = int(response[:3])
+        response_code = int(response.json()[:3])
         self.assertEqual(200, response_code)
 
         # Delete item just added to DB
@@ -25,7 +25,7 @@ class TestShortenUrlAPI(unittest.TestCase):
     def test_custom_url_with_collision(self):
         custom_url = "existing_url"
         response = client.post(url="/api/shorten_url", json={"custom_url": custom_url, "original_url": "https://www.existing.com"})
-        response_code = int(response[:3])
+        response_code = int(response.json()[:3])
         self.assertEqual(409, response_code)
 
         # Delete item just added to DB
