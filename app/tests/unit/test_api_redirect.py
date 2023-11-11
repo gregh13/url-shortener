0be07555df542cd3cb9b014b9667db1c9e4ece5b
Greg <greg.herringer@gmail.com>
2023-11-10 16:07:20 -0800

    update test params and response formatting

--- app/tests/unit/test_api_redirect.py
+++ app/tests/unit/test_api_redirect.py
@@ -10,13 +10,19 @@ class TestRedirectAPI(unittest.TestCase):
         valid_url = "existing_url"
         url = f"/api/redirect/{valid_url}"
         response = client.get(url=url)
+
+        self.assertEqual("https://www.google.com", response.url)
         self.assertEqual(200, response.status_code)
 
     def test_non_existing_url_redirect(self):
         invalid_url = "non_existing_url"
         url = f"/api/redirect/{invalid_url}"
         response = client.get(url=url)
-        self.assertEqual(404, response.json()["status_code"])
+        # get response gives 200 even with invalid url, need to access database response dictionary
+        response_status_code = response.json()["status_code"]
+
+        self.assertEqual("http://testserver/api/redirect/non_existing_url", response.url)
+        self.assertEqual(404, response_status_code)
 
     def test_no_input_redirect(self):
         no_input = ""
