aa054f809c748a9439b742f1117e5999b4297ed5
Gregory Herringer <greg.herringer@gmail.com>
2023-12-11 15:36:03 -0500

    add basic User class

--- app/models/pydantic_models.py
+++ app/models/pydantic_models.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class PostURL(BaseModel):
     original_url: str
     custom_url: str | None = None
+
+
+class User(BaseModel):
+    email: str
+    admin: bool | None = False
+    disabled: bool | None = False
+
